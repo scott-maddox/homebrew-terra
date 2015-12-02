405a9a74667a1d75b27c5fb9d310be0fdd3930b7
Scott Maddox <smaddox@utexas.edu>
2015-12-01 20:05:54 -0600

    fix sha256 hash

--- terra.rb
+++ terra.rb
@@ -3,7 +3,7 @@ class Terra < Formula
   homepage "http://terralang.org/"
   url "https://github.com/zdevito/terra/releases/download/release-2015-08-03/terra-OSX-x86_64-84bbb0b.zip"
   version "2015.08.03"
-  sha256 = "53b2473629c8ec03046d189b7ebfa13b12c2dabc5f0b2731fe7e4cc4abf7c82d"
+  sha256 "53b2473629c8ec03046d189b7ebfa13b12c2dabc5f0b2731fe7e4cc4abf7c82d"
 
   def install
 
