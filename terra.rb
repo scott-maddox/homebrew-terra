5dba9a2cbcf3ac4ec7df4a77a39dc1d8b93bc16f
Simon Walker <s.r.walker101@googlemail.com>
2017-03-04 16:06:42 +0000

    Update terra version to 2016-02-26

--- terra.rb
+++ terra.rb
@@ -1,9 +1,9 @@
 class Terra < Formula
   desc "a new low-level system programming language"
   homepage "http://terralang.org/"
-  url "https://github.com/zdevito/terra/releases/download/release-2015-08-03/terra-OSX-x86_64-84bbb0b.zip"
-  version "2015.08.03"
-  sha256 "53b2473629c8ec03046d189b7ebfa13b12c2dabc5f0b2731fe7e4cc4abf7c82d"
+  url "https://github.com/zdevito/terra/releases/download/release-2016-02-26/terra-OSX-x86_64-2fa8d0a.zip"
+  version "2016.02.26"
+  sha256 "648e4f656c0b023dbba581b1dd27450c10f0d11608d8a7222bc1cfe3a13532fc"
 
   def install
 
